b7df8fe9aa07af699f8f03e15b2b82f36ce00dc6
tflagg3 <112423819+tflagg3@users.noreply.github.com>
2022-10-11 10:45:14 -0600

    Add files via upload

--- src/networkLayer/NewNetwork.java
+++ src/networkLayer/NewNetwork.java
@@ -17,6 +17,8 @@ public class NewNetwork extends Network{
     @Override
     public void receiveFromTransport(byte[] p) {
         //lets assume p is passed in as a packet already
+        //the first byte will be the addressTo
+        byte to = p[0];
         for(Link l: links){
             //if this link is addressed to the same one as the metadata expects it to be
             //then send the message on this link
@@ -30,7 +32,10 @@ public class NewNetwork extends Network{
 
     @Override
     public byte[] toRawBytes(NetworkPacket p) {
-        byte[] output = ((Meta) p.meta()).getData() + p.data();
+        byte[] output = new byte[p.data().length + ((Meta) p.meta()).getData().length];
+        Meta meta = (Meta) p.meta();
+        System.arraycopy(meta.getData(), 0, output, 0, meta.getData().length);
+        System.arraycopy(p.data(), 0, output, meta.getData().length, p.data().length);
         return output;
     }
 
